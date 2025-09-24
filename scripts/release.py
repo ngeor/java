67359624a252c35ae4120eec1ec930eb9db69838
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2025-09-24 14:24:31 +0200

    feat!: Support custom location for `keys.asc` and `pom.xml`
    Breaking change: the parameter `--gpg-key` is renamed to `--gpg-key-name`
    
    The new mandatory parameter `--gpg-key-file` points to the key file

--- scripts/release.py
+++ scripts/release.py
@@ -14,33 +14,34 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Creates a release of a Maven library")
-    parser.add_argument("--gpg-key", help="GPG key", required=True)
+    parser.add_argument("--gpg-key-file", help="Location of the GPG key", required=True)
+    parser.add_argument("--gpg-key-name", help="GPG key name", required=True)
     parser.add_argument("--gpg-passphrase", help="GPG passphrase", required=True)
     parser.add_argument(
         "--maven-username",
-        help="The username to use for publishing the release to Maven",
+        help="Maven Central repository username",
         required=True,
     )
     parser.add_argument(
         "--maven-password",
-        help="The password to use for publishing the release to Maven",
+        help="Maven Central repository password",
         required=True,
     )
+    parser.add_argument("-f", "--file", help="Location of pom.xml file", required=False)
+
     return parser.parse_args()
 
 
 def release(args):
     try:
         gpg_list_keys()
-        gpg_import_key(args.gpg_passphrase, "scripts/keys.asc")
-        perform_release(
-            args.gpg_key, args.gpg_passphrase, args.maven_username, args.maven_password
-        )
+        gpg_import_key(args.gpg_passphrase, args.gpg_key_file)
+        perform_release(args)
     finally:
-        clean_gpg(args.gpg_key)
+        clean_gpg(args.gpg_key_name)
 
 
-def clean_gpg(gpg_key):
+def clean_gpg(gpg_key_name):
     # gpg -K | grep "^  " | tr -d " " | xargs gpg --batch --yes --delete-secret-keys
     # gpg --batch --yes --delete-key ${GPG_KEY}
     p1 = subprocess.run(
@@ -53,10 +54,12 @@ def clean_gpg(gpg_key):
                 ["gpg", "--batch", "--yes", "--delete-secret-keys", key.strip()],
                 check=True,
             )
-    subprocess.run(["gpg", "--batch", "--yes", "--delete-key", gpg_key], check=True)
+    subprocess.run(
+        ["gpg", "--batch", "--yes", "--delete-key", gpg_key_name], check=True
+    )
 
 
-def perform_release(gpg_key, gpg_passphrase, maven_username, maven_password):
+def perform_release(args):
     with tempfile.TemporaryDirectory() as tmp_dir:
         settings_xml_file = os.path.join(tmp_dir, "settings.xml")
         with open(settings_xml_file, "w") as f:
@@ -66,8 +69,8 @@ def perform_release(gpg_key, gpg_passphrase, maven_username, maven_password):
     <servers>
         <server>
             <id>central</id>
-            <username>{maven_username}</username>
-            <password>{maven_password}</password>
+            <username>{args.maven_username}</username>
+            <password>{args.maven_password}</password>
         </server>
     </servers>
 </settings>
@@ -77,32 +80,35 @@ def perform_release(gpg_key, gpg_passphrase, maven_username, maven_password):
         # as per https://maven.apache.org/plugins/maven-gpg-plugin/sign-mojo.html
         # that is the best practice and the gpg.passphrase property is deprecated
         new_env = os.environ.copy()
-        new_env["MAVEN_GPG_PASSPHRASE"] = gpg_passphrase
+        new_env["MAVEN_GPG_PASSPHRASE"] = args.gpg_passphrase
+        maven_args = [
+            "mvn",
+            "-ntp",
+            "-B",
+            "-s",
+            settings_xml_file,
+            # skip surefire tests
+            "-DskipTests=true",
+            # skip failsafe tests
+            "-DskipITs=true",
+            # skip checkstyle
+            "-Dcheckstyle.skip=true",
+            # skip jacoco
+            "-Djacoco.skip=true",
+            # skip invoker
+            "-Dinvoker.skip=true",
+            # skip spotless
+            "-Dspotless.check.skip=true",
+            # skip sortpom
+            "-Dsort.skip=true",
+            "-Pgpg",
+            f"-Dgpg.keyname={args.gpg_key_name}",
+        ]
+        if args.file:
+            maven_args += ["-f", args.file]
+        maven_args += ["deploy"]
         subprocess.run(
-            [
-                "mvn",
-                "-ntp",
-                "-B",
-                "-s",
-                settings_xml_file,
-                # skip surefire tests
-                "-DskipTests=true",
-                # skip failsafe tests
-                "-DskipITs=true",
-                # skip checkstyle
-                "-Dcheckstyle.skip=true",
-                # skip jacoco
-                "-Djacoco.skip=true",
-                # skip invoker
-                "-Dinvoker.skip=true",
-                # skip spotless
-                "-Dspotless.check.skip=true",
-                # skip sortpom
-                "-Dsort.skip=true",
-                "-Pgpg",
-                f"-Dgpg.keyname={gpg_key}",
-                "deploy",
-            ],
+            maven_args,
             check=True,
             env=new_env,
         )
@@ -116,7 +122,7 @@ def gpg_list_keys():
     subprocess.run(["gpg", "--list-keys"], check=True)
 
 
-def gpg_import_key(gpg_passphrase, key_file):
+def gpg_import_key(gpg_passphrase, gpg_key_file):
     """
     Imports the GPG key.
     Equivalent shell:
@@ -130,7 +136,7 @@ def gpg_import_key(gpg_passphrase, key_file):
             f"--passphrase={gpg_passphrase}",
             "--output",
             "-",
-            key_file,
+            gpg_key_file,
         ],
         stdout=subprocess.PIPE,
     )
