7eb4953385986ee2aa8ddcca55f418bab33b851b
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2025-11-15 18:52:13 +0100

    feat: support filtering pom.xml for some interesting symbols

--- scripts/test-repo.sh
+++ scripts/test-repo.sh
@@ -4,6 +4,27 @@
 #
 # find . -type d -depth 1 -exec ./java/scripts/test-repo.sh {} \; -print
 
+POM_TOKENS=()
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+    --sonatype )
+        POM_TOKENS+=("sonatype")
+        ;;
+    --main-class )
+        POM_TOKENS+=("<mainClass>")
+        ;;
+    --graal )
+        POM_TOKENS+=("graal")
+        ;;
+    --tomcat )
+        POM_TOKENS+=("tomcat")
+        ;;
+    --spring )
+        POM_TOKENS+=("spring")
+        ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
 if [[ ! -d "$1/.git" ]]; then
     exit 1
 fi
@@ -11,3 +32,10 @@ fi
 if [[ ! -f "$1/pom.xml" ]]; then
     exit 2
 fi
+
+for pom_token in "${POM_TOKENS[@]}"; do
+    grep -q "$pom_token" "$1/pom.xml"
+    if [[ $? -ne 0 ]]; then
+        exit 3
+    fi
+done
