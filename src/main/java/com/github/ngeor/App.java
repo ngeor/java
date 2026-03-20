2684ab00612b71699d9f0724c146f630dfda4484
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2026-03-20 21:17:49 +0100

    Add sample picocli app

--- src/main/java/com/github/ngeor/App.java
+++ src/main/java/com/github/ngeor/App.java
@@ -1,9 +1,19 @@
 package com.github.ngeor;
 
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 /**
  * Hello world!
  */
-public final class App {
+@Command(name = "app", mixinStandardHelpOptions = true, version = "1.0",
+    description = "A sample Picocli application.")
+public final class App implements Callable<Integer> {
+    @Option(names = "--dry-run", description = "A dummy dry-run argument.")
+    private boolean dryRun;
+
     private App() {}
 
     /**
@@ -11,6 +21,12 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.exit(new CommandLine(new App()).execute(args));
+    }
+
+    @Override
+    public Integer call() {
+        System.out.println("Hello World! dryRun was " + dryRun);
+        return 0;
     }
 }
